aa39ff6c451db20df22b191197dc6481425d0f5f
boldyrevn <boldyrev.now@mail.ru>
2023-11-23 13:24:30 +0300

    module is ready

--- config_load.go
+++ config_load.go
@@ -1,1 +1,41 @@
 package hw_server_config
+
+import (
+    "gopkg.in/yaml.v3"
+    "io"
+    "os"
+    "strconv"
+    "time"
+)
+
+type innerConfig struct {
+    Host    string `yaml:"host"`
+    Port    int    `yaml:"port"`
+    Timeout int    `yaml:"timeout"`
+}
+
+type ServerConfig struct {
+    Addr    string
+    Timeout time.Duration
+}
+
+// LoadYamlConfig makes server config from .yaml file. Timeout must be specified in milliseconds
+func LoadYamlConfig(path string) (*ServerConfig, error) {
+    var ic innerConfig
+    file, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    b, err := io.ReadAll(file)
+    if err != nil {
+        return nil, err
+    }
+    err = yaml.Unmarshal(b, &ic)
+    if err != nil {
+        return nil, err
+    }
+    return &ServerConfig{
+        Addr:    ic.Host + ":" + strconv.Itoa(ic.Port),
+        Timeout: time.Millisecond * time.Duration(ic.Timeout),
+    }, nil
+}
--- config_load_test.go
+++ config_load_test.go
@@ -1,1 +1,24 @@
 package hw_server_config
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+    "time"
+)
+
+func TestLoadYamlConfig(t *testing.T) {
+    t.Run("valid config", func(t *testing.T) {
+        s, err := LoadYamlConfig("./test_configs/config.yaml")
+        assert.NoError(t, err)
+        assert.Equal(t, "localhost:8080", s.Addr)
+        assert.Equal(t, time.Millisecond*20, s.Timeout)
+    })
+    t.Run("invalid config path", func(t *testing.T) {
+        _, err := LoadYamlConfig("./confi_1.yaml")
+        assert.Error(t, err)
+    })
+    t.Run("invalid config format", func(t *testing.T) {
+        _, err := LoadYamlConfig("./test_configs/config_wrong.yaml")
+        assert.Error(t, err)
+    })
+}
